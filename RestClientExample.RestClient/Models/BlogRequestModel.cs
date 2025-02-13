cbf3f8ec77ce01b13732e765de71ebb491653919
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-13 20:45:58 +0630

    Format Code

--- RestClientExample.RestClient/Models/BlogRequestModel.cs
+++ RestClientExample.RestClient/Models/BlogRequestModel.cs
@@ -1,12 +1,11 @@
-﻿namespace RestClientExample.RestClient.Models
+﻿namespace RestClientExample.RestClient.Models;
+
+public class BlogRequestModel
 {
-	public class BlogRequestModel
-	{
-		public string BlogTitle { get; set; } = null!;
+	public string BlogTitle { get; set; } = null!;
 
-		public string BlogAuthor { get; set; } = null!;
+	public string BlogAuthor { get; set; } = null!;
 
-		public string BlogContent {  get; set; } = null!;
+	public string BlogContent {  get; set; } = null!;
 
-	}
 }
