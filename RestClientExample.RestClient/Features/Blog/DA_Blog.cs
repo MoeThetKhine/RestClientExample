099d67d1397bb32c40bd24d19cc08f265ce960f8
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-13 22:44:33 +0630

    Feat: Add GetBlogs in DA_Blog

--- RestClientExample.RestClient/Features/Blog/DA_Blog.cs
+++ RestClientExample.RestClient/Features/Blog/DA_Blog.cs
@@ -2,5 +2,46 @@
 {
 	public class DA_Blog
 	{
+		private readonly AppDbContext _appDbContext;
+
+		public DA_Blog(AppDbContext appDbContext)
+		{
+			_appDbContext = appDbContext;
+		}
+
+		public async Task<BlogListResponseModel> GetBlogs(int pageNo, int pageSize)
+		{
+			var lst = await _appDbContext.Blogs.AsNoTracking()
+				.OrderByDescending(x => x.BlogId)
+				.Skip((pageNo - 1) * pageSize)
+				.Take(pageSize)
+				.ToListAsync();
+
+			var rowCount = await _appDbContext.Blogs.CountAsync();
+			var pageCount = rowCount / pageSize;
+
+			if(rowCount % pageSize > 0)
+			{
+				pageCount++;
+			}
+
+			PageSettingModel pageSettingModel = new()
+			{
+				PageCount = pageCount,
+				PageSize = pageSize,
+				PageNo = pageNo,
+			};
+
+			return new BlogListResponseModel()
+			{
+				Data = new BlogDataModel() { Blogs = lst },
+				PageSetting = new PageSettingModel()
+				{
+					PageCount = pageCount,
+					PageSize = pageSize,
+					PageNo = pageNo,
+				}
+			};
+		}
 	}
 }
