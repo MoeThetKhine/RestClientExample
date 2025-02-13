063e2ce64524c32ff576e0a14d8043edfb670511
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-13 23:03:35 +0630

    Feat : Add GetBlog in BL_Blog

--- RestClientExample.RestClient/Features/Blog/BL_Blog.cs
+++ RestClientExample.RestClient/Features/Blog/BL_Blog.cs
@@ -2,5 +2,19 @@
 {
 	public class BL_Blog
 	{
+		private readonly DA_Blog _dA_Blog;
+
+		public BL_Blog(DA_Blog dA_Blog)
+		{
+			_dA_Blog = dA_Blog;
+		}
+
+		public async Task<BlogListResponseModel> GetBlog(int pageNo, int pageSize)
+		{
+			if (pageNo == 0 || pageSize == 0)
+				throw new Exception("Invalid Request.");
+
+			return await _dA_Blog.GetBlogs(pageNo, pageSize);
+		}
 	}
 }
