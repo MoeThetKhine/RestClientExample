ef067cf51f0b4155f5e04d6d9b9d33bf8d78470f
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-13 23:11:13 +0630

    Format Code

--- RestClientExample.RestClient/Features/Blog/BL_Blog.cs
+++ RestClientExample.RestClient/Features/Blog/BL_Blog.cs
@@ -1,24 +1,23 @@
-﻿namespace RestClientExample.RestClient.Features.Blog
+﻿namespace RestClientExample.RestClient.Features.Blog;
+
+public class BL_Blog
 {
-	public class BL_Blog
+	private readonly DA_Blog _dA_Blog;
+
+	public BL_Blog(DA_Blog dA_Blog)
 	{
-		private readonly DA_Blog _dA_Blog;
-
-		public BL_Blog(DA_Blog dA_Blog)
-		{
-			_dA_Blog = dA_Blog;
-		}
-
-		#region GetBlog
-
-		public async Task<BlogListResponseModel> GetBlog(int pageNo, int pageSize)
-		{
-			if (pageNo == 0 || pageSize == 0)
-				throw new Exception("Invalid Request.");
-
-			return await _dA_Blog.GetBlogs(pageNo, pageSize);
-		}
-
-		#endregion
+		_dA_Blog = dA_Blog;
 	}
+
+	#region GetBlog
+
+	public async Task<BlogListResponseModel> GetBlog(int pageNo, int pageSize)
+	{
+		if (pageNo == 0 || pageSize == 0)
+			throw new Exception("Invalid Request.");
+
+		return await _dA_Blog.GetBlogs(pageNo, pageSize);
+	}
+
+	#endregion
 }
