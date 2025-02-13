3148835163c88bf1d925ac75943fc60bbff616d9
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-13 23:09:14 +0630

    Feat : Add GetBlogs

--- RestClientExample.RestClient/Features/Blog/BlogController.cs
+++ RestClientExample.RestClient/Features/Blog/BlogController.cs
@@ -7,5 +7,35 @@ namespace RestClientExample.RestClient.Features.Blog
 	[ApiController]
 	public class BlogController : ControllerBase
 	{
+		private readonly BL_Blog _bL_Blog;
+
+		public BlogController(BL_Blog bL_Blog)
+		{
+			_bL_Blog = bL_Blog;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetBlogs(int pageNo , int pageSize)
+		{
+			try
+			{
+				BlogListResponseModel lst = await _bL_Blog.GetBlog(pageNo, pageSize);
+				return Ok(new ResponseModel()
+				{
+					IsSuccess = true,
+					Data = lst.Data,
+					PageSetting = lst.PageSetting,
+				});
+			}
+
+			catch(Exception ex)
+			{
+				return BadRequest(new ResponseModel()
+				{
+					IsSuccess = false,
+					Message = ex.Message
+				});
+			}
+		}
 	}
 }
