4f27457366c0a0ccf0733dc2578fdc1969a5d953
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-13 21:30:41 +0630

    Feat : Add AddBusinessLogicServices

--- RestClientExample.RestClient/ModularService.cs
+++ RestClientExample.RestClient/ModularService.cs
@@ -1,6 +1,17 @@
-﻿namespace RestClientExample.RestClient
+﻿using RestClientExample.RestClient.Features.Blog;
+
+namespace RestClientExample.RestClient
 {
-	public class ModularService
+	public static class ModularService
 	{
+		
+		public static IServiceCollection AddBusinessLogicServices(this  IServiceCollection services)
+		{
+			services.AddScoped<BL_Blog>();
+			return services;
+		}
+
+		
+
 	}
 }
