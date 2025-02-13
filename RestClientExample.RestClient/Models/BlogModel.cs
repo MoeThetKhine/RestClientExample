9a3a9223ca0d94daa53402794cf493ea36230e01
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-13 18:21:48 +0630

    Feat : Add Blog Model

--- RestClientExample.RestClient/Models/BlogModel.cs
+++ RestClientExample.RestClient/Models/BlogModel.cs
@@ -1,6 +1,15 @@
-﻿namespace RestClientExample.RestClient.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace RestClientExample.RestClient.Models
 {
+	[Table("Tbl_Blog")]
 	public class BlogModel
 	{
+		[Key]
+		public int BlogId {  get; set; }
+		public string BlogTitle { get; set; } = null!;
+		public string BlogAuthor { get; set; } = null!;
+		public string BlogContent { get; set; } = null!;
 	}
 }
