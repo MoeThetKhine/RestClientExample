d6809fc0aa7f177ea5d5830360e83547c80bc088
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-13 20:10:48 +0630

    -

--- RestClientExample.RestClient/Models/PageSettingModel.cs
+++ RestClientExample.RestClient/Models/PageSettingModel.cs
@@ -1,8 +1,12 @@
 ﻿namespace RestClientExample.RestClient.Models;
 
+#region PageSettingModel
+
 public class PageSettingModel
 {
 	public int PageCount { get; set; }	
 	public int PageNo { get; set; }
 	public int PageSize {  get; set; }
 }
+
+#endregion
