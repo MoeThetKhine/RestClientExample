bd370437abc4922478d4b840d0a92b2de64a3560
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-13 23:12:28 +0630

    Format Code

--- RestClientExample.RestClient/Features/Blog/DA_Blog.cs
+++ RestClientExample.RestClient/Features/Blog/DA_Blog.cs
@@ -1,47 +1,46 @@
-﻿namespace RestClientExample.RestClient.Features.Blog
-{
-	public class DA_Blog
-	{
-		private readonly AppDbContext _appDbContext;
+﻿namespace RestClientExample.RestClient.Features.Blog;
 
-		public DA_Blog(AppDbContext appDbContext)
+public class DA_Blog
+{
+	private readonly AppDbContext _appDbContext;
+
+	public DA_Blog(AppDbContext appDbContext)
+	{
+		_appDbContext = appDbContext;
+	}
+
+	public async Task<BlogListResponseModel> GetBlogs(int pageNo, int pageSize)
+	{
+		var lst = await _appDbContext.Blogs.AsNoTracking()
+			.OrderByDescending(x => x.BlogId)
+			.Skip((pageNo - 1) * pageSize)
+			.Take(pageSize)
+			.ToListAsync();
+
+		var rowCount = await _appDbContext.Blogs.CountAsync();
+		var pageCount = rowCount / pageSize;
+
+		if(rowCount % pageSize > 0)
 		{
-			_appDbContext = appDbContext;
+			pageCount++;
 		}
 
-		public async Task<BlogListResponseModel> GetBlogs(int pageNo, int pageSize)
+		PageSettingModel pageSettingModel = new()
 		{
-			var lst = await _appDbContext.Blogs.AsNoTracking()
-				.OrderByDescending(x => x.BlogId)
-				.Skip((pageNo - 1) * pageSize)
-				.Take(pageSize)
-				.ToListAsync();
+			PageCount = pageCount,
+			PageSize = pageSize,
+			PageNo = pageNo,
+		};
 
-			var rowCount = await _appDbContext.Blogs.CountAsync();
-			var pageCount = rowCount / pageSize;
-
-			if(rowCount % pageSize > 0)
-			{
-				pageCount++;
-			}
-
-			PageSettingModel pageSettingModel = new()
+		return new BlogListResponseModel()
+		{
+			Data = new BlogDataModel() { Blogs = lst },
+			PageSetting = new PageSettingModel()
 			{
 				PageCount = pageCount,
 				PageSize = pageSize,
 				PageNo = pageNo,
-			};
-
-			return new BlogListResponseModel()
-			{
-				Data = new BlogDataModel() { Blogs = lst },
-				PageSetting = new PageSettingModel()
-				{
-					PageCount = pageCount,
-					PageSize = pageSize,
-					PageNo = pageNo,
-				}
-			};
-		}
+			}
+		};
 	}
 }
