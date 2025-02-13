276a0fad7af7ca329e17ffadc10d4ab89fd35874
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-13 20:09:33 +0630

    Feat : Add ResponseModel

--- RestClientExample.RestClient/Models/ResponseModel.cs
+++ RestClientExample.RestClient/Models/ResponseModel.cs
@@ -2,5 +2,11 @@
 {
 	public class ResponseModel
 	{
+		public bool IsSuccess {  get; set; }
+		public bool IsError { get { return !IsSuccess; } }
+		public string? Message {  get; set; }
+		public object? Data { get; set; }
+		public object? Item { get; set; }
+		public PageSettingModel PageSetting { get; set; }
 	}
 }
